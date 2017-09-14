e353343271daba1eda553c3427e3af3550859db8
abhishekguptamzn <30700661+abhishekguptamzn@users.noreply.github.com>
2017-09-14 15:46:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,55 @@
 console.log('Loaded!');
+
+var button = document.getElementById('counter');
+button.onclick = function() {
+
+	//Create a request 
+	var request = new XMLHttpRequest();
+	console.log(request);
+	//Capture the response
+	request.onreadystatechange = function() {
+if (request.readyState === XMLHttpRequest.DONE) {
+
+	if (request.status === 200) {
+		var counter = request.responseText;
+		var span = document.getElementById('count');
+	span.innerHTML = counter.toString();
+	}
+}
+	};
+	request.open('GET','/counter',true);
+	request.send(null);
+
+
+}
+
+//Submit Name 
+var submitBtn = document.getElementById('namesubmit');
+submitBtn.onclick = function() {
+
+var request = new XMLHttpRequest();
+
+	//Capture the response
+	request.onreadystatechange = function() {
+if (request.readyState === XMLHttpRequest.DONE) {
+
+	if (request.status === 200) {
+		var names = request.responseText;
+		names = JSON.parse(names);
+	var list = '';
+	for (var i =0; i <names.length; i++) {
+		list+='<li>'+names[i]+'</li>';
+	}
+	var ul = document.getElementById('namelist');
+	ul.innerHTML = list;
+
+
+	}
+}
+	};
+	var nameInput = document.getElementById('name');
+var name = nameInput.value;
+	console.log(name);
+	request.open('GET','/name-save?name=' + name,true);
+	request.send(null);
+}
